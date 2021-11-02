638628a9368ceeb1f9831b49c5ef4fd7248d1822
Alankar Shukla <alankarshukla9@gmail.com>
2021-11-02 17:22:58 +0530

    All files updated from the repo

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "DVC_NLP_Simple_Usecase"
-AUTHOR_USER_NAME = "alan-ai-learner"
+REPO_NAME = "DVC-NLP-Simple-usecase"
+AUTHOR_USER_NAME = "c17hawke"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -14,7 +14,7 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="A small package for DVC-NLP",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
